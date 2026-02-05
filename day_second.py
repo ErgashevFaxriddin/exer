736b9e7ed14797f56fc81de75f68948743f36112
faxriddin ergashev <uzcodermelody@gmail.com>
2026-02-05 10:59:30 +0500

    string methods

--- day_second.py
+++ day_second.py
@@ -1,5 +1,27 @@
-num = -9
-if num > 0:
-    print('--->')
-else:
-    print('<---')
+# num = -9
+# if num > 0:
+#     print('--->')
+# else:
+#     print('<---')
+
+# word = input('write any word: \n>>>')
+# print(f"you wrote {len(word)} letters")
+
+# sentence = input('write any sentence: ')
+# words = sentence.split()
+# print("you wrote", len(words), "words")
+
+print("WELCOME TO THE GAME😊")
+while True:
+
+    sentence = input('WRITE ANY SENTENCES: ')
+    words = sentence.split()
+    print("YOU WROTE", len(words), "WORDS")
+
+    again = input('WANNA PLAY AGAIN? (YES/NO)')
+
+    if again == 'YES'.lower():
+        continue
+    else:
+        print('THANK YOU!🫶')
+        break
