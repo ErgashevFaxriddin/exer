6a1c4f71b683af217c2464e1e7465ebd689fff04
faxriddin ergashev <uzcodermelody@gmail.com>
2026-02-07 18:25:27 +0500

    authorization

--- day4.py
+++ day4.py
@@ -1,15 +1,17 @@
 import sqlite3
 
 conn = sqlite3.connect('data4.db')
-c = conn.execute("""
+c = conn.cursor()
+
+c.execute("""
 CREATE TABLE IF NOT EXISTS Teachers(
     name TEXT,
     age INTEGER,
     subject TEXT
 )
 """)
-
 conn.commit()
 
 def add_teacher(name, age, subject):
     c.execute('INSERT INTO Teachers VALUES(?, ?, ?)', (name, age, subject))
+    conn.commit()
--- main4.py
+++ main4.py
@@ -1,10 +1,25 @@
-from day4 import *
+from day4 import add_teacher, conn
 
-name = input('YOUR NAME: '.title())
-age = int(input('YOUR AGE: '))
-subject = input('YOUR SUBJECT: ')
+while True:
+    name = input('YOUR NAME: ')
 
-add_teacher(name, age, subject)
+    while True:
+        age = input('YOUR AGE: ')
+        if age.isdigit():
+            age = int(age)
+            break
+        else:
+            print("PLEASE, ENTER YOUR AGE: ")
+
+    subject = input('YOUR SUBJECT: ')
+
+    add_teacher(name, age, subject)
+
+    ask = int(input('RO\'YXATNI DAVOM ETTIRASIZMI? \nHa: 1 \nYo\'q: 0\n>>>'))
+    if ask == 1:
+        continue
+    else:
+        break
 
 conn.close()
 print('SAVED!')
