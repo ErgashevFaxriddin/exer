8cf812058a7cffc6ce3c7e0eb9a8af2685640d75
faxriddin ergashev <uzcodermelody@gmail.com>
2026-02-09 22:54:29 +0500

    third exercise

---
+++ day6.py
@@ -0,0 +1,11 @@
+# add = lambda a, b : a + b
+# print(add(1,2))
+
+# multiply = lambda a, b: a *b
+# x = int(input('first digit: '))
+# y = int(input('second digit: '))
+# print(x, 'x', y, '=', multiply(x, y))
+
+check = lambda x: 'positive' if x>0 else 'negativ or zero'
+n = int(input('enter a number: '))
+print(check(n))
